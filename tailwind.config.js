48558aeffc21de52ef924af59d7d070b427081ac
Bruno Sampaio <sampaio.bruno9@gmail.com>
2023-08-27 15:58:31 -0400

    Adicionando o Tailwind css e começando a fazer o front-end do sistema/2

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      preto: '#000000',
+      branco: '#FFFFF',
+      brancoMedio: '#F8F7F6',
+      cinzaClaro: '#D9D8D7',
+      cinzaMedio: '#AEAAA6',
+      cinzaEscuro: '#707070',
+      verdeClaro: '#99B3A3',
+      VerdeMedio: '#308D54',
+      VerdeEscuro: '#1A8C46',
+      azulClaro: '#98B8D0',
+      azulCinza: '#8D9AAE',
+      azulEscuro: '#47799D',
+      // ...
+    },
+  },
+  plugins: [],
+};
