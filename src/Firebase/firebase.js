a2934ab4a4ceafefc98e2a87b7067550f08018e2
Bruno Sampaio <dev.brunno9@gmail.com>
2024-01-16 21:11:45 -0400

    Iniciando uma nova etapa nesse projeto

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAO8d-MYanZe1vspfPqvQ7LY8bxDXvIcJo',
-  authDomain: 'calcwebapp.firebaseapp.com',
-  projectId: 'calcwebapp',
-  storageBucket: 'calcwebapp.appspot.com',
-  messagingSenderId: '78399243621',
-  appId: '1:78399243621:web:1224cc1cc0e762c6594d8c',
+  apiKey: "AIzaSyCx1yveiisT0t3thk4IBnhtS2UqiPEhwb0",
+  authDomain: "calc-trabalhista.firebaseapp.com",
+  projectId: "calc-trabalhista",
+  storageBucket: "calc-trabalhista.appspot.com",
+  messagingSenderId: "771723194366",
+  appId: "1:771723194366:web:2d7bd633d72704a8b8daaf",
+  measurementId: "G-4XVSZT0PLX"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
